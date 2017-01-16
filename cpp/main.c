b593f1faac9f7f692538fa0d7a9862618822348a
Tommy Lindman <tommy5545@gmail.com>
2017-01-16 09:43:03 +0100

    Started setting up SDL.

--- cpp/main.c
+++ cpp/main.c
@@ -1,15 +1,50 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
 #include "chip8.h"
 
 using namespace std;
+
+static int keymap[0x10] = {
+    SDLK_0,
+    SDLK_1,
+    SDLK_2,
+    SDLK_3,
+    SDLK_4,
+    SDLK_5,
+    SDLK_6,
+    SDLK_7,
+    SDLK_8,
+    SDLK_9,
+    SDLK_a,
+    SDLK_b,
+    SDLK_c,
+    SDLK_d,
+    SDLK_e,
+    SDLK_f
+};
+
+SDL_Window * screen;
+
+void setupGraphics()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    //SDL_SetVideoMode(640, 320, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+    screen = SDL_CreateWindow("Chip-8 Interpreter",
+			      SDL_WINDOWPOS_CENTERED,
+			      SDL_WINDOWPOS_CENTERED,
+			      640,
+			      320,
+			      0);
+}
+
 
 int main()
 {
     Chip8 chip8;
     
     // Set up render system and register input callbacks
-    // setupGraphics();
+    setupGraphics();
     // setupInput();
 
     // Initialize the Chip8 system and load the game into memory
